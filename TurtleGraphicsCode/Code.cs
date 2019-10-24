b80b900796f50287a04f24eea232b042f2de3e61
Vojtěch Kořínek <vojtech.korinek@arbesova.local>
2019-10-24 17:00:07 +0200

    snad to funguje a doma to oteveru

--- TurtleGraphicsCode/Code.cs
+++ TurtleGraphicsCode/Code.cs
@@ -1,33 +1,48 @@
 ﻿using System;
 
-namespace TurtleGraphicsCode {
+namespace TurtleGraphicsCode
+{
 
-	public class Code {
+    public class Code
+    {
 
-		/// <summary>
-		/// This is the place to put your turtle code
-		/// </summary>
-		public Turtle ToExecute() {
-			Turtle t = new Turtle();
-			t.Rotate(-90);
+        /// <summary>
+        /// This is the place to put your turtle code
+        /// </summary>
+        public Turtle ToExecute()
+        {
+            Turtle t = new Turtle();
+            t.Rotate(-90);
+            for (int i = 0; i < 6; i++)
+            {
 
-			for (int i = 0; i < 20; i++) {
-				DrawSection(t, 100);
-			}
-			return t;
-		}
+                t.StoreTurtlePosition();
+                for (int j = 0; j < 20; j++)
+                {
+                    DrawSection(t, 100);
+                }
+                t.RestoreTurtlePosition();
+                t.Rotate(60);
+            
+            }
+            return t;
+        }
 
-		void DrawSection(Turtle t, int p) {
-			t.Forward(10);
-			t.StoreTurtlePosition();
-			t.Rotate(-45);
-			t.Forward(p);
-			t.RestoreTurtlePosition();
-			t.Rotate(45);
-			t.Forward(p);
-			t.RestoreTurtlePosition(true);
-		}
-	}
+
+
+
+        void DrawSection(Turtle t, int p)
+        {
+            t.Forward(10);
+            t.StoreTurtlePosition();
+            t.Rotate(-45);
+            t.Forward(p);
+            t.RestoreTurtlePosition();
+            t.Rotate(45);
+            t.Forward(p);
+            t.RestoreTurtlePosition(true);
+        }
+    }
 }
 
 
@@ -42,10 +57,3 @@ namespace TurtleGraphicsCode {
 
 
 
-//Vytvořím si složku na disku Z: na mé projekty ve Visual Studiu
-//Otevřu cmd/powershell v složce z řádku 11
-//Zadám 'git clone https://github.com/Michal-MK/TurtleGraphics.git'
-//Ovetřu stažený solution ve visual studiu(.sln soubor)
-//Najdu projekt 'TurteGraphicsCode'
-//Najdu soubor Code.cs a otevřu ho
-//Jsem zde!
